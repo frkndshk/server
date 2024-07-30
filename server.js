638ff1149cea4aa45cc1d3977e949bb5a4141c0b
frkndshk <155109614+frkndshk@users.noreply.github.com>
2024-07-30 05:31:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3002;
 var yedekler = null;
+
 const connection = mysql.createConnection({
     host: process.env.DB_HOST || 'brflrxzq5tg82kflfbmv-mysql.services.clever-cloud.com',
     user: process.env.DB_USER || 'uqcw9bdlanbwc4sn',
@@ -15,28 +16,43 @@ const connection = mysql.createConnection({
 // CORS middleware
 app.use(cors());
 
-// Veritabanı bağlantı olaylarını izleme
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// VeritabanÄ± baÄŸlantÄ± olaylarÄ±nÄ± izleme
 connection.connect(function (err) {
     if (err) {
         console.error('Error connecting to MySQL:', err);
         return;
     }
-
     console.log('Connected to MySQL');
 });
 
+// Verileri al
 app.get('/api/menuler', (req, res) => {
-    connection.query('SELECT * FROM menuler', (error, results, fields) => {
+    connection.query('SELECT * FROM beo_menu', (error, results, fields) => {
         if (error) {
             console.error('Error querying the database:', error);
             if (yedekler) {
-                res.json(yedekler)
+                res.json(yedekler);
             }
             return;
         }
-
         res.json(results);
         yedekler = results;
+    });
+});
+
+// Verileri kaydet
+app.post('/api/menuler', (req, res) => {
+    const jsonData = JSON.stringify(req.body);
+
+    connection.query('INSERT INTO boe_menu (json_data) VALUES (?)', [jsonData], (error, results) => {
+        if (error) {
+            console.error('Error inserting into the database:', error);
+            return res.status(500).send('Error inserting data');
+        }
+        res.status(200).send(`JSON kaydedildi! ID: ${results.insertId}`);
     });
 });
 
